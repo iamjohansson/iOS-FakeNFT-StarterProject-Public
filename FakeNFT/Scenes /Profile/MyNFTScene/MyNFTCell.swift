034f3c92be411b114493b316c69f619eb8a49eb2
iamjohansson <ma4ujloba@gmail.com>
2024-01-28 17:25:40 +0300

    Доработка конфигурации ячейки моих нфт

--- FakeNFT/Scenes /Profile/MyNFTScene/MyNFTCell.swift
+++ FakeNFT/Scenes /Profile/MyNFTScene/MyNFTCell.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class MyNFTCell: UITableViewCell {
     
-    // MARK: - Properties & UI Elements
+    // MARK: Properties & UI Elements
+    private var idNft: String?
+    
     private lazy var image: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -35,7 +38,11 @@ final class MyNFTCell: UITableViewCell {
         return label
     }()
     
-    // TODO: UI Rating
+    private lazy var ratingView: DynamicRatingView = {
+        let view = DynamicRatingView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     private lazy var ownerLabel: UILabel = {
         let label = UILabel()
@@ -62,6 +69,22 @@ final class MyNFTCell: UITableViewCell {
         return label
     }()
     
+    private lazy var middleStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 4
+        stack.axis = .vertical
+        return stack
+    }()
+    
+    private lazy var rightStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 2
+        stack.axis = .vertical
+        return stack
+    }()
+    
     // MARK: Lifecycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -75,12 +98,46 @@ final class MyNFTCell: UITableViewCell {
     }
     
     // MARK: Methods
-    private func addSubView() {
+    func configureCell(nft: NFTModel) {
+        self.idNft = nft.id
         
+        if let imageURL = nft.images.first,
+           let url = URL(string: imageURL) {
+            image.kf.setImage(with: url)
+        }
+        
+        nameLabel.text = nft.name
+        ratingView.configureRating(nft.rating)
+        ownerLabel.text = "От \(nft.author)"
+        priceLabel.text = "\(nft.price) ETH"
+    }
+    func configureLikeInCell(isLiked: Bool) {
+        let imageName = isLiked ? "likeActive" : "likeNotActive"
+        likeButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    
+    private func addSubView() {
+        [nameLabel, ratingView, ownerLabel].forEach { middleStack.addArrangedSubview($0) }
+        [priceNameLabel, priceLabel].forEach { rightStack.addArrangedSubview($0) }
+        [image, likeButton, middleStack, rightStack].forEach { contentView.addSubview($0) }
     }
     
     private func applyConstraint() {
-        
+        NSLayoutConstraint.activate([
+            image.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.baseOffset16),
+            image.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.baseOffset16),
+            image.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Constants.baseOffset16),
+            image.widthAnchor.constraint(equalToConstant: Constants.imageSize),
+            image.heightAnchor.constraint(equalToConstant: Constants.imageSize),
+            likeButton.topAnchor.constraint(equalTo: image.topAnchor),
+            likeButton.trailingAnchor.constraint(equalTo: image.trailingAnchor),
+            likeButton.heightAnchor.constraint(equalToConstant: Constants.likeSize),
+            likeButton.widthAnchor.constraint(equalToConstant: Constants.likeSize),
+            middleStack.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            middleStack.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: Constants.baseOffset20),
+            rightStack.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            rightStack.leadingAnchor.constraint(equalTo: middleStack.trailingAnchor, constant: Constants.baseOffset39)
+        ])
     }
     
     // MARK: Actions
@@ -89,3 +146,12 @@ final class MyNFTCell: UITableViewCell {
     }
 }
 
+private extension MyNFTCell {
+    enum Constants {
+        static let baseOffset16: CGFloat = 16
+        static let baseOffset20: CGFloat = 20
+        static let baseOffset39: CGFloat = 39
+        static let imageSize: CGFloat = 108
+        static let likeSize: CGFloat = 42
+    }
+}
