1247c2ebceb0dda706a0645e6ddb6e4eb1ad00ac
iamjohansson <ma4ujloba@gmail.com>
2024-02-03 17:26:11 +0300

    Вернулись к тестовому урлу и обновили его работу.

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,12 +1,8 @@
 enum RequestConstants {
     #warning("insert your baseUrl <id>.mockapi.io")
-    static let baseURL = URLType.mock
+    static let baseURL = URLType.test
 }
 enum URLType {
     static let mock = "https://64858e8ba795d24810b71189.mockapi.io"
     static let test = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
 }
-/*
- Для тестинга работоспособности необходимо использовать Mock URL:
- В тестовом URL отсутствует возможность подгрузить Мои NFT через Postman(не проходит оплата ордера), приходят наполовину нерабочие NFT с одинаковыми аватарами, валюта и рейтинг координально отличаются от макета.
- */
